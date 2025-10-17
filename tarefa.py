b2fa57d34690c38ad093739f6de9711d096cf678
luizromanodev <only.dev2005@gmail.com>
2025-10-17 01:19:59 -0300

    feat: refactor sterp

--- tarefa.py
+++ tarefa.py
@@ -2,6 +2,10 @@ class GerenciadorDeTarefas:
     def __init__(self):
         self.tarefas = []
         
+    def _validar_indice(self, indice):
+        if not (0 <= indice < len(self.tarefas)):
+            raise IndexError("Tarefa Inexistente")
+        
     def adicionar_tarefa(self, nome, descricao):
         
         if not nome:
@@ -15,17 +19,18 @@ class GerenciadorDeTarefas:
         self.tarefas.append(nova_tarefa)
         
     def marcar_como_concluida(self, indice_tarefa):
+        self._validar_indice(indice_tarefa)
         if self.tarefas[indice_tarefa]['status'] == "concluída":
             raise ValueError("A tarefa ja esta concluída")
         self.tarefas[indice_tarefa]['status'] = "concluída"
         
     def marcar_como_em_andamento(self, indice_tarefa):
+        self._validar_indice(indice_tarefa)
         if self.tarefas[indice_tarefa]['status'] == "em andamento":
             raise ValueError("Não é possivel alterar o status de uma tarefa concluida")
         self.tarefas[indice_tarefa]['status'] = "em andamento"
         
     def editar_tarefa(self, indice_tarefa, novo_nome, nova_descricao):
-        if not (0 <= indice_tarefa < len(self.tarefas)):
-            raise IndexError("Tarefa Inexistente")
+        self._validar_indice(indice_tarefa)
         self.tarefas[indice_tarefa]['nome'] = novo_nome
         self.tarefas[indice_tarefa]['descricao'] = nova_descricao
